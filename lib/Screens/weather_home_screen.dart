f95778af00a6dd4fc90cf2fe076251ba87b604a1
abdelrahman <abdomorad602@gmail.com>
2024-08-26 12:38:10 +0300

    UI for Account Screen & explore screen done

--- lib/Screens/weather_home_screen.dart
+++ lib/Screens/weather_home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:untitled2/Screens/Explore_Screen.dart';
+import 'package:untitled2/Screens/acount_screen.dart';
+import 'package:untitled2/Screens/home_screen.dart';
 
 class WeatherHomeScreen extends StatefulWidget {
   const WeatherHomeScreen({super.key});
@@ -8,24 +11,19 @@ class WeatherHomeScreen extends StatefulWidget {
 }
 
 class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
+  List Screens = [ExploreScreen(),HomeScreen(),Acount_Screen() ];
   int _currentIndex = 0; // State variable to track the selected tab index.
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text("Weather Information"),
-          ],
-        ),
-      ),
+
+      body:Screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.yellow,
+        unselectedLabelStyle: TextStyle(color: Colors.yellow ,fontWeight: FontWeight.w700 ,fontSize: 20),
+        selectedFontSize: 20,
+        iconSize: 20,
+        backgroundColor: Colors.black,
         currentIndex: _currentIndex, // Set the current index.
         onTap: (index) {
           setState(() {
@@ -34,15 +32,16 @@ class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
         },
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
+
+            icon: Icon(Icons.explore,color: Colors.yellow,),
             label: 'Explore',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.details),
-            label: 'Details',
+            icon: Icon(Icons.home_filled,color: Colors.yellow,),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person,color: Colors.yellow,),
             label: 'Profile',
           ),
         ],
